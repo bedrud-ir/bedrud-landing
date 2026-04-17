ed612f65fdd87d12a8873aa0420b15848d6eeea5
James Landers <275338146+JamesLuctly@users.noreply.github.com>
2026-04-18 01:26:38 +0330

    update branding to bedrud-ir and RAM spec from 200MB to 512MB

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
-export const GITHUB_REPO = "themadorg/bedrud";
+export const GITHUB_REPO = "bedrud-ir/bedrud";
 export const GITHUB_URL = `https://github.com/${GITHUB_REPO}`;
 export const GITHUB_DISCUSSIONS_URL = `${GITHUB_URL}/discussions`;
 export const DOCS_URL = "https://bedrud.org/en/docs";
